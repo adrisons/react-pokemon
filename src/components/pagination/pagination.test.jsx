bad49a396fd02ac70db8654a191ce309fb144e44
Adrián Pereira Guerra <adries.91@gmail.com>
2021-07-11 12:47:00 +0200

    test: pagination component

---
+++ src/components/pagination/pagination.test.jsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import renderer from "react-test-renderer";
+import Pagination from "./pagination";
+
+describe("WHEN: Pagination with no prev page link", () => {
+  const component = renderer.create(<Pagination gotoNextPage="next-page" />);
+  const tree = component.toJSON();
+  it("THEN: should match snapshot", () => {
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("THEN: component should not display prev page button", () => {
+    expect(tree.children.length).toEqual(1);
+  });
+});
+describe("WHEN: Pagination with prev page link", () => {
+  const component = renderer.create(
+    <Pagination gotoNextPage="next-page" gotoPrevPage="prev-page" />
+  );
+  const tree = component.toJSON();
+  it("THEN: should match snapshot", () => {
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("THEN: component should display prev page button", () => {
+    expect(tree.children.length).toEqual(2);
+  });
+});
